7c9b9fe00bfaa03d4d59a7493fd1a25c3c2bbdbd
huangjipeng <1319639755@qq.com>
2022-03-24 16:13:09 +0800

    chore: ejs模板几种特殊用法

--- bin/ejs/index.js
+++ bin/ejs/index.js
@@ -1,4 +1,5 @@
 const ejs = require('ejs')
+const fs = require('fs');
 const path = require('path')
 //第一种方法
 const html ='<div><%= user.name%></div>'
@@ -19,6 +20,15 @@ console.log(compileTemplate)   //<div>liugezhou</div>
 const renderTemplate = ejs.render(html,data,options)
 console.log(renderTemplate)
 
+let myFileLoader = function (filePath) {
+  return 'myFileLoader: ' + fs.readFileSync(filePath).toString(); // 在所有加载的 ejs 模板前，加上 myFileLoader 字符串
+};
+
+ejs.fileLoader = myFileLoader; // 通过 myFileLoader 去加载文件
+
 //第三种用法
 const renderFile = ejs.renderFile(path.resolve(__dirname,'test.html'),data,options)
 renderFile.then(file => console.log(file))
+
+
+
